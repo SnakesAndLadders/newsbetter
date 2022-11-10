c6d8d2488a50688222271c577f0855e4da7e835f
Shawn Ayotte <shawn.ayotte@cybera.ca>
2022-11-10 11:15:29 -0700

    Minor Version Update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as readme_fp:
 
 setup(
     name="NewsBetter",
-    version="1.0.2",
+    version="1.0.3",
     author="Shawn Ayotte",
     author_email="shawn.k.ayotte@gmail.com",
     description="Yet another RSS news reader ",

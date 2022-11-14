20a36293fdc36f8d9df5e4c593c7430569034798
Shawn Ayotte <shawn.ayotte@cybera.ca>
2022-11-14 16:56:06 -0700

    Update article parsing

--- NewsBetter/__init__.py
+++ NewsBetter/__init__.py
@@ -6,6 +6,7 @@ Created:
 
 import os
 import textwrap
+import json
 import sys
 from pathlib import Path
 import py_cui
@@ -14,11 +15,17 @@ from newspaper import Article, Config
 
 __version__ = 'v1.0.3'
 
-myfile = Path(str(Path.home()) + '/.urls')
-myfile.touch(exist_ok=True)
+urlfile = Path(str(Path.home()) + '/.nburls')
+urlfile.touch(exist_ok=True)
+
+settingsfile = Path(str(Path.home()) + '/.nbsettings')
+settingsfile.touch(exist_ok=True)
+
 config = Config()
 config.browser_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124  Safari/537.36'
 config.fetch_images = False
+
+columns, rows = os.get_terminal_size(0)
 
 class NewsbetterApp:
 
@@ -60,7 +67,7 @@ class NewsbetterApp:
         self.article_read.add_key_command(               py_cui.keys.KEY_LEFT_ARROW, self.back_to_articles)
 
     def load_feeds(self):
-        with open(myfile, 'r+') as f:
+        with open(urlfile, 'r+') as f:
             for line in f:
                 (key, val) = line.split(",")
                 self.d[key] = val
@@ -83,13 +90,13 @@ class NewsbetterApp:
             self.name = name + " 2"
         else:
             self.name = name
-        file_object = open(myfile, 'a')
+        file_object = open(urlfile, 'a')
         file_object.write(self.name + ",")
         file_object.close()
         self.add_new_popup = self.root.show_text_box_popup("URL:", self.save_new)
 
     def save_new(self, entry):
-        file_object = open(myfile, 'a')
+        file_object = open(urlfile, 'a')
         file_object.write(entry.strip() + "\n")
         file_object.close()
         self.name = ""
@@ -128,7 +135,7 @@ class NewsbetterApp:
         self.root.move_focus(self.feed_list)
 
     def read_article(self):
-        columns, rows = os.get_terminal_size(0)
+        
         article_title = self.article_list.get()
         try:
             for i in self.feed.entries:
@@ -138,21 +145,8 @@ class NewsbetterApp:
                         article.download()
                         article.parse()
 
-            if self.summary == 1:
-                try:
-                    article.nlp()
-                    text_wrap = textwrap.wrap(article.summary, width=columns - 5, drop_whitespace=False, replace_whitespace=False)
-                    self.article_read.set_title(article_title + " (Summary)")
-                except Exception as e:
-                    text_wrap = textwrap.wrap(article.text, width=columns - 5, drop_whitespace=False, replace_whitespace=False)
-                    self.article_read.set_title(article_title + " (Summary not available " + str(e) + ")")
-            elif self.summary == 0:
-                text_wrap = textwrap.wrap(article.text, width=columns - 5, drop_whitespace=False, replace_whitespace=False)
-                self.article_read.set_title(article_title)
-
             text = "\nPublish Date: " + str(article.publish_date) + "\n"
-            for text_line in text_wrap:
-                text += text_line.rstrip() + "\n"
+            text = text + parse_article(article.text)
 
         except Exception as e:
             text = "sorry, this page failed to load. Please try another article. " + str(e)
@@ -161,8 +155,29 @@ class NewsbetterApp:
         self.article_read.set_text(str(text))
         self.root.move_focus(self.article_read)
 
+def parse_article(text):
+    f = open(settingsfile)
+    data = json.load(f)
+    f.close()
+    settings = tuple(data.items())
+    removeme = settings[0][1]
+    removephrase = settings[1][1]
+
+    inputtext = text.splitlines()
+    output = ""
+    for line in inputtext:
+        if not line.startswith(tuple(removeme)) and line != "":
+            for old in removephrase:
+                line = line.replace(old, "")
+
+            output = output + "\n"
+            wrapped = textwrap.wrap(line, width=columns - 5, drop_whitespace=True, replace_whitespace=False)
+            for newline in wrapped:
+                output += newline + "\n"
+    return(output)
 
 def main():
     root = py_cui.PyCUI(9, 8)
     wrapper =  NewsbetterApp(root)
     root.start()
+main()
